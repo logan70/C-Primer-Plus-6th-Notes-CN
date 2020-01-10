f581bfe68a1ac34c5a961d3548844b5d7ba056a1
Logan <im.lizhh@gmail.com>
2020-01-10 11:50:33 +0800

    chore: add command to do preparatory work for learning next chapter

---
+++ build/ready-to-learn-next-chapter.js
@@ -0,0 +1,30 @@
+/**
+ * Do some preparatory works for learning next chapter.
+ */
+const fs = require('fs')
+const {
+  chapterCNNameMap,
+  allChapters,
+  exampleDirName,
+  exerciseDirName,
+  imagesDirName,
+  reviewQuestionsDirName,
+} = require('./constant')
+const {
+  getChapterDirs,
+  resolve,
+} = require('./utils')
+ 
+main()
+
+function main() {
+  const chaptersAlreadyLearned = getChapterDirs()
+  const chaptersNotLearned = allChapters.filter(chapter => !chaptersAlreadyLearned.includes(chapter))
+  const nextChapter = chaptersNotLearned[0]
+  fs.mkdirSync(resolve(nextChapter)) // make root dir of next chapter
+  fs.mkdirSync(resolve(nextChapter, exampleDirName)) // make example dir of next chapter
+  fs.mkdirSync(resolve(nextChapter, exerciseDirName)) // make programming-exercise dir of next chapter
+  fs.mkdirSync(resolve(nextChapter, imagesDirName)) // make images dir of next chapter
+  fs.mkdirSync(resolve(nextChapter, reviewQuestionsDirName)) // make review-questions dir of next chapter
+  fs.writeFileSync(resolve(nextChapter, 'README.md'), `# ${chapterCNNameMap[nextChapter]}\n\n`)// generate READMD.md
+}
