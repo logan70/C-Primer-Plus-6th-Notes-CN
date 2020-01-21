f72484a8b363371ecee06f3c96cc7de29c63d3d7
Logan <im.lizhh@gmail.com>
2020-01-21 16:28:32 +0800

    learning chapter 4(day 2)

---
+++ Chapter-4 Character Strings and Formatted Input-Output/example/4.11-intconv.c
@@ -0,0 +1,15 @@
+// intconv.c 一些不匹配的整形转换
+#include <stdio.h>
+#define PAGES 336
+#define WORDS 65618
+
+int main(void)
+{
+  short num = PAGES;
+  short mnum = -PAGES;
+  printf("num as short and unsigned short: %hd %hu\n", num, num);
+  printf("-num as short and unsigned short: %hd %hu\n", mnum, mnum);
+  printf("num as int and char: %d %c\n", num, num);
+  printf("WORDS as int, short, and char: %d %hd %c\n", WORDS, WORDS, WORDS);
+  return 0;
+}
