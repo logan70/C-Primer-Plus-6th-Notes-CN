56508f5cac1c9bfc24c9edf61fd4522c5dbd8bce
Logan <im.lizhh@gmail.com>
2020-02-05 12:29:09 +0800

    learning chapter 5(day 1)

---
+++ Chapter-5 Operators, Expressions, and Statements/example/5.1-shoes1.c
@@ -0,0 +1,14 @@
+// shoes1.c - 把鞋码换成英寸
+#include <stdio.h>
+#define ADJUST 7.31
+
+int main(void)
+{
+  const double SCALE = 0.333; // const变量
+  double shoe, foot;
+  shoe = 9.0;
+  foot = SCALE * shoe + ADJUST;
+  printf("Shoe size(men's) foot length\n");
+  printf("%10.1f %15.2f inches\n", shoe, foot);
+  return 0;
+}
